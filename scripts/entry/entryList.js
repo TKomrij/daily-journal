9751d10dd4878fe0ec406594b0d106e3d16b3d60
TKomrij <tatiane.b.komrij@gmail.com>
2020-12-01 15:45:47 -0500

    json to render entries

--- scripts/entry/entryList.js
+++ scripts/entry/entryList.js
@@ -1,15 +1,16 @@
-import {useEntry} from "./entryDataProvider.js"
+import {useEntry, getEntries} from "./entryDataProvider.js"
 import {Entry} from "./entry.js"
 
-
-
-
 export const entryList = () => {
-  const contentElement = document.querySelector(".entries")
-  const allTheEntries = useEntry()
-
-  for (const entryObject of allTheEntries) {
-    const entryHTML = Entry(entryObject)
-    contentElement.innerHTML += entryHTML
-  }
+    getEntries().then(
+      () => {
+        const contentElement = document.querySelector(".entries")
+        const allTheEntries = useEntry()
+        for (const entryObject of allTheEntries) {
+          const entryHTML = Entry(entryObject)
+        // Add to the existing HTML in the content element
+        contentElement.innerHTML += entryHTML
+      }
+      }
+    )
 }
